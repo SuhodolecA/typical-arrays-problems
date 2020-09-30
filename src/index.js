ce1a6b8e09b40687750429f0a31de022f3a4ba21
Anton Sukhadolets <Kut18@tut.by>
2020-09-30 17:31:42 +0300

    refactor: add another approach

--- src/index.js
+++ src/index.js
@@ -1,38 +1,49 @@
 
-exports.min = function min (array) {
-	if(!array || array.length == []) {
-		return 0;
-	}
-    let min = array[0];
-	for(let i=0; i<array.length; i++){
-		if(array[i]<min) {
-			min = array[i];
-		}
-	}
-  return min;
+exports.min = function min(array) {
+    // 	if(!array || array.length == []) {
+    // 		return 0;
+    // 	}
+    //     let min = array[0];
+    // 	for(let i=0; i<array.length; i++){
+    // 		if(array[i]<min) {
+    // 			min = array[i];
+    // 		}
+    // 	}
+    //   return min;
+    if (!array || array.length < 1) {
+        return 0;
+    }
+
+    return array.sort((a, b) => a - b)[0];
 };
 
-exports.max = function max (array) {
-	if(!array || array.length == []) {
-		return 0;
-	}
-    let max = array[0];
-	for(i=0; i<array.length; i++){
-		if(array[i]>max) {
-			max = array[i];
-		}
-	}
-  return max;
+exports.max = function max(array) {
+    // 	if(!array || array.length == []) {
+    // 		return 0;
+    // 	}
+    //     let max = array[0];
+    // 	for(i=0; i<array.length; i++){
+    // 		if(array[i]>max) {
+    // 			max = array[i];
+    // 		}
+    // 	}
+    //   return max;
+    if (!array || array.length < 1) {
+        return 0;
+    };
+    return array.sort((a, b) => b - a)[0];
 };
 
-exports.avg = function avg (array) {
-	if(!array || array.length == []) {
-		return 0;
-	}
-	let avg = array.reduce((a,b)=>{
-  	return a+b;
-  });
-  return avg/array.length;
+exports.avg = function avg(array) {
+    if (!array || array.length < 1) {
+        return 0;
+    }
+    let length =  array.length;
+    let avg = array.reduce((a, b) => {
+        return a + b;
+    });
+    
+    return avg / length;
 };
 
 // exports.avg = function avg (array) {
